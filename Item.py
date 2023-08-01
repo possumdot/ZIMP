e043daba88451ceae8c95f230683926b5fa17a0d
GrumpyPossum <sheltrme@gmail.com>
2023-08-02 11:54:01 +1200

    Revert "Work done in class"
    This reverts commit 6f10db7ea7a011e2212ad953d9d9531b4f10ed5c.

--- Item.py
+++
@@ -1,7 +0,0 @@
-class Item:
-    def __init__(self):
-        self.name = None
-        self.description = None
-        self.attack_modifier = 0
-        self.health_modifier = 0
-
--- TheGame.py
+++
@@ -1,8 +0,0 @@
-import Player
-
-class TheGame:
-    def __init__(self):
-        time = 2100
-
-    def add_player(self):
-        player = Player
