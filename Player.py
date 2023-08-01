b41abb0bd6fcd6d430daa83cdf98e545392b1db0
GrumpyPossum <sheltrme@gmail.com>
2023-08-02 11:57:00 +1200

    first commit

---
+++ Player.py
@@ -0,0 +1,32 @@
+"""
+Players have a starting health of 6 and a starting attack of 1
+Players can only hold 2 items at a time
+"""
+
+
+class Player:
+    def __init__(self):
+        self.health = 6
+        self.attack = 1
+        self.item_1 = None
+        self.item_2 = None
+
+    def heal(self, amount):
+        self.health += amount
+
+    def take_damage(self, amount):
+        self.health -= amount
+
+    def drop_item(self, item_slot):
+        match item_slot:
+            case 1:
+                self.item_1 = None
+            case 2:
+                self.item_2 = None
+
+    def give_item(self, item, item_slot):
+        match item_slot:
+            case 1:
+                self.item_1 = item
+            case 2:
+                self.item_2 = item
---
+++ main.py
@@ -0,0 +1,14 @@
+"""
+
+"""
+import TheGame
+
+"""
+Starts the game
+"""
+def main():
+    the_game = TheGame
+
+
+if __name__ == '__main__':
+    main()
