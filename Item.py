6f10db7ea7a011e2212ad953d9d9531b4f10ed5c
GrumpyPossum <sheltrme@gmail.com>
2023-08-02 11:48:46 +1200

    Work done in class

---
+++ Item.py
@@ -0,0 +1,7 @@
+class Item:
+    def __init__(self):
+        self.name = None
+        self.description = None
+        self.attack_modifier = 0
+        self.health_modifier = 0
+
---
+++ TheGame.py
@@ -0,0 +1,8 @@
+import Player
+
+class TheGame:
+    def __init__(self):
+        time = 2100
+
+    def add_player(self):
+        player = Player
