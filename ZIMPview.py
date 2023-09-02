6855c36e6379018ed5093c0bfc6ba8493a1eba1f
possumdot <82570621+possumdot@users.noreply.github.com>
2023-09-02 13:33:30 +1200

    Create ZIMPview.py
    Framework for the view for a CMD based ZIMP game

---
+++ ZIMPview.py
@@ -0,0 +1,47 @@
+from cmd import Cmd
+
+class ZIMPview(Cmd):
+    __init__(self):
+        self.prompt=">>> "
+
+    def do_start(self, args):
+      pass
+
+    def do_restart(self, args):
+      pass
+
+    def do_quit(self, args):
+      pass
+
+    def do_load(self, args):
+      pass
+
+    def do_save(self, args):
+      pass
+
+    def do_rules(self, args):
+      pass
+
+    def do_move(self, args):
+      pass
+
+  #might not be needed considering a room might fit just fine
+    def do_rotate(self, args):
+      pass
+
+    def do_fight(self, args):
+      pass
+
+    def do_cower(self, args):
+      pass
+
+    def do_use(self, args):
+      """uses an item if you have it on your character"""
+      pass
+
+    def do_pickup(self, args):
+      """picks an item off the ground and replaces it with the item in the arg, could also be item slot"""
+      pass
+
+    def do_burytotem(self, args):
+      pass
